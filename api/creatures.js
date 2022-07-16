1b620b25b063f4a3246cedb21dad6f63321d1eb0
emiliacarotti <99696866+emiliacarotti@users.noreply.github.com>
2022-07-16 11:23:24 -0400

    Feat  backend #5  get all creatures (#7)
    * feat backend #5 get all creatures
    
    * feat: backend #5 updates to api files

--- api/creatures.js
+++ api/creatures.js
@@ -10,29 +10,34 @@ const {
   getCreaturebyCategory,
 } = require("../db");
 
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage worked')
+})
+
+
 creaturesRouter.get("/", async (req, res, next) => {
-  try {
-    const allCreatures = await getAllCreatures();
+  // try {
+  //   const allCreatures = await getAllCreatures();
 
-    const creatures = allCreatures.filter((creature) => {
-      // the post is active, doesn't matter who it belongs to
-      if (post.active) {
-        return true;
-      }
+  //   const creatures = allCreatures.filter((creature) => {
+  //     // the post is active, doesn't matter who it belongs to
+  //     if (post.active) {
+  //       return true;
+  //     }
 
-      // the post is not active, but it belogs to the current user
-      if (req.user && post.author.id === req.user.id) {
-        return true;
-      }
+  //     // the post is not active, but it belogs to the current user
+  //     if (req.user && post.author.id === req.user.id) {
+  //       return true;
+  //     }
 
-      // none of the above are true
-      return false;
-    });
+  //     // none of the above are true
+  //     return false;
+  //   });
 
-    res.send({
-      creatures,
-    });
-  } catch ({ name, message }) {
-    next({ name, message });
-  }
+  //   res.send({
+  //     creatures,
+  //   });
+  // } catch ({ name, message }) {
+  //   next({ name, message });
+  // }
 });
