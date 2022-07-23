997869b3baa9392322db94834f6ede69b8c8e409
Dreedy2 <99706095+Dreedy2@users.noreply.github.com>
2022-07-23 13:40:20 -0400

    style: created about page, fixed nav links

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,36 @@ import reactdomclient from "react-dom/client"
 import { BrowserRouter, useNavigate, useParams, Routes, Route, Link } from "react-router-dom";
 
 function About(){
-    return <p>About</p>
+    return  <section id="about">
+                <div class="about-1">
+                    <h2>About Us</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis cras sed felis eget velit aliquet sagittis id.</p>
+                </div>
+                <div id="about-2">
+                    <div class="content-box-lg">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="about-item">
+                                        <i class="fa fa-book"></i>
+                                        <h3>Monster Mission</h3>
+                                        <hr/>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis cras sed felis eget velit aliquet sagittis id.</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="about-item">
+                                        <i class="fa fa-globe"></i>
+                                        <h3>Where We Are</h3>
+                                        <hr/>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis cras sed felis eget velit aliquet sagittis id.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </section>
 }
 
 export default About;
