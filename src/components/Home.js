d86a412c408dc4cb90edce8bd4e7ac3205c54961
arich9504 <99704305+arich9504@users.noreply.github.com>
2022-07-28 22:15:41 -0400

    feat: building home (getCreatures)

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,49 @@ import reactdomclient from "react-dom/client"
 import { BrowserRouter, useNavigate, useParams, Routes, Route, Link } from "react-router-dom";
 
 
-function Home(){
-    return <p>Home</p>
+export default function Home({creatures, setCreatures, selectedCreature, setSelectedCreature, isAdmin, setIsAdmin}){
+    
+    useEffect(()=>{
+        async function getCreatures(){
+            try{
+
+                const response = await fetch('http://localhost:4000/api/creatures', {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+
+                },
+                })
+                let data = await response.json()
+                console.log("data******:",data.allCreatures)
+                setCreatures(data.allCreatures)
+
+            }catch(err){
+                console.log(err)
+            }
+
+        }
+
+        getCreatures()
+    },[])
+
+    
+    return ( 
+        <>
+           {/* <div>
+      {creatures.map((creatures) => {
+        return (
+          <div key={creatures.creatureid}>
+            <div>{creatures.name}</div>
+            <div>{creatures.price}</div> 
+            </div> */}
+        </>
+    )
+
+
+    
+        
+
 }
 
-export default Home;
+
--- src/index.js
+++ src/index.js
@@ -35,7 +35,9 @@ function MadDog() {
     const [token, setToken] = useState("");
     const [profile, setProfile] = useState([]);
     const [username, setUsername] = useState("");
-    const [isAdmin, setIsAdmin] = useState(false); 
+    const [isAdmin, setIsAdmin] = useState(false);  
+    const [creatures, setCreatures] = useState([]);
+    const [selectedCreature, setSelectedCreature] = useState({})
     const navigate = useNavigate();
   
     useEffect(() => {
@@ -93,7 +95,16 @@ function MadDog() {
           ></Route>
           <Route
             path="/"
-            element={<Home profile={profile} setProfile={setProfile} />}
+            element={
+              <Home 
+                creatures={creatures} 
+                setCreatures={setCreatures} 
+                isAdmin={isAdmin}
+                setIsAdmin={setIsAdmin}
+                selectedCreature={selectedCreature}
+                setSelectedCreature={setSelectedCreature}
+              />
+            }
           ></Route>
 
 
