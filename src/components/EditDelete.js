6628a5515f1de50782bc91870eef6c6a4dc90fec
harley215 <harley215h@gmail.com>
2022-07-30 09:48:57 -0400

    fix: edit creature moved to Edit/Delete component

--- src/components/EditDelete.js
+++ src/components/EditDelete.js
@@ -2,6 +2,15 @@ import React, {useState, useEffect} from "react";
 import reactdomclient from "react-dom/client"
 import { BrowserRouter, useNavigate, useParams, Routes, Route, Link } from "react-router-dom";
 
+/**HARLEY'S EFFORTS 
+     const [creatureName, setCreatureName] = useState('');
+    const [creaturePrice, setCreaturePrice] = useState('');
+    const [creatureStock, setCreatureStock] = useState('');
+    const [creatureEnvironment, setCreatureEnvironment] = useState('');
+    const [creatureSize, setCreatureSize] = useState('');
+    const [creatureFood, setCreatureFood] = useState('');
+    const [creatureTemper, setCreatureTemper] = useState('');
+ */
 
 export default function EditDeleteCreature({
     creatureid,
@@ -139,3 +148,52 @@ return (
         //BUTTONS SHOULD ONLY SHOW FOR ADMIN
 
 )}  
+
+/* HARLEYS EFFORTS --------------------------------------
+return (
+  <>
+
+      <div className="singleCreature">
+          <div><h4>Name:{creatures.title}</h4></div>
+          <div>Price: {creatures.price}</div>
+          <div>Stock:{creatures.stock}</div>
+          <div>Environment:{creatures.environment}</div>
+          <div>Size:{creatures.size}</div>
+          <div>Food:{creatures.food}</div>
+          <div>Temperment:{creatures.temper}</div>
+      </div>
+
+      {/*EDIT CREATURE */}/*
+      <form id={`editCreature${creatureid}`} className="editCreatureForm" onSubmit={async (event) => {
+          event.preventDefault();
+          const result = await editCreature(token, isAdmin, creatureName, creaturePrice, creatureStock, creatureEnvironment, creatureSize, creatureFood,creatureTemper);
+          if (!result.error) {
+              getCreaturebyId();
+
+          } else {
+              alert(result.error);
+          }
+      }}>
+          <input type="text" placeholder="name" onChange={(event) => { setCreatureName(event.target.value) }}></input>
+          <br></br>
+          <input type="text" placeholder="price" onChange={(event) => { setCreaturePrice(event.target.value) }}></input>
+          <br></br>
+          <input type="text" placeholder="stock" onChange={(event) => { setCreatureStock(event.target.value) }}></input>
+          <br></br>
+          <input type="text" placeholder="environment" onChange={(event) => { setCreatureEnvironment(event.target.value) }}></input>
+          <br></br>
+          <input type="text" placeholder="size" onChange={(event) => { setCreatureSize(event.target.value) }}></input>
+          <br></br>
+          <input type="text" placeholder="food" onChange={(event) => { setCreatureFood(event.target.value) }}></input>
+          <br></br>
+          <input type="text" placeholder="temper" onChange={(event) => { setCreatureTemper(event.target.value) }}></input>
+          <br></br>
+
+          <button type="submit" className="editCreature">Submit Changes</button>
+      </form>
+          <button className="deleteCreature" onClick={() => { deleteCreature(token, creatureid) }}>Delete Creature</button>
+  </>
+)
+}
+
+*/
--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -7,13 +7,7 @@ import { BrowserRouter, useNavigate, useParams, Routes, Route, Link } from "reac
 export default function SingleItem({ creatures, setCreatures, selectedCreature, setSelectedCreature, isAdmin, setIsAdmin }) {
     let imgURL = "/creature"
 
-    const [creatureName, setCreatureName] = useState('');
-    const [creaturePrice, setCreaturePrice] = useState('');
-    const [creatureStock, setCreatureStock] = useState('');
-    const [creatureEnvironment, setCreatureEnvironment] = useState('');
-    const [creatureSize, setCreatureSize] = useState('');
-    const [creatureFood, setCreatureFood] = useState('');
-    const [creatureTemper, setCreatureTemper] = useState('');
+
     
 
 
@@ -48,46 +42,7 @@ export default function SingleItem({ creatures, setCreatures, selectedCreature, 
 
     return (
         <>
-
-            <div className="singleCreature">
-                <div><h4>Name:{creatures.title}</h4></div>
-                <div>Price: {creatures.price}</div>
-                <div>Stock:{creatures.stock}</div>
-                <div>Environment:{creatures.environment}</div>
-                <div>Size:{creatures.size}</div>
-                <div>Food:{creatures.food}</div>
-                <div>Temperment:{creatures.temper}</div>
-            </div>
-
-            {/*EDIT CREATURE */}
-            <form id={`editCreature${creatureid}`} className="editCreatureForm" onSubmit={async (event) => {
-                event.preventDefault();
-                const result = await editCreature(token, isAdmin, creatureName, creaturePrice, creatureStock, creatureEnvironment, creatureSize, creatureFood,creatureTemper);
-                if (!result.error) {
-                    getCreaturebyId();
-
-                } else {
-                    alert(result.error);
-                }
-            }}>
-                <input type="text" placeholder="name" onChange={(event) => { setCreatureName(event.target.value) }}></input>
-                <br></br>
-                <input type="text" placeholder="price" onChange={(event) => { setCreaturePrice(event.target.value) }}></input>
-                <br></br>
-                <input type="text" placeholder="stock" onChange={(event) => { setCreatureStock(event.target.value) }}></input>
-                <br></br>
-                <input type="text" placeholder="environment" onChange={(event) => { setCreatureEnvironment(event.target.value) }}></input>
-                <br></br>
-                <input type="text" placeholder="size" onChange={(event) => { setCreatureSize(event.target.value) }}></input>
-                <br></br>
-                <input type="text" placeholder="food" onChange={(event) => { setCreatureFood(event.target.value) }}></input>
-                <br></br>
-                <input type="text" placeholder="temper" onChange={(event) => { setCreatureTemper(event.target.value) }}></input>
-                <br></br>
-
-                <button type="submit" className="editCreature">Submit Changes</button>
-            </form>
-                <button className="deleteCreature" onClick={() => { deleteCreature(token, creatureid) }}>Delete Creature</button>
+        <p>Signle Item Page</p> 
         </>
     )
 }
