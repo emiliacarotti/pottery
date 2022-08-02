3c4d0e3c73078c8ab95e8681be6cb4bae3cbcceb
arich9504 <99704305+arich9504@users.noreply.github.com>
2022-08-01 20:27:22 -0400

    feat: create creature function adjusted

--- db/creature.js
+++ db/creature.js
@@ -14,18 +14,18 @@ async function getAllCreatures() {
   }
 }
 
-async function createCreature({ name, price, stock, environment, size, food, temper}) {
-    try {
-        const { rows } = await client.query(`
-        INSERT INTO creature(name, price, stock, environment, size, food, temper)
-       VALUES ($1,$2,$3,$4,$5,$6,$7)
-       RETURNING *;
-      `,[name, price, stock, environment, size, food, temper]);
-      console.log(rows)
-        return rows;
-    } catch (error) {
-      console.error("Error creating creatures!"+error);
-    }
+async function createCreature({creatureid, name, price, stock, environment, size, food, temper}) {
+  try {
+      const { rows } = await client.query(`
+     INSERT INTO creature(name, price, stock, environment, size, food, temper)
+     VALUES ($1,$2,$3,$4,$5,$6,$7)
+     RETURNING *;
+    `,[ name, price, stock, environment, size, food, temper]);
+    //console.log(rows)
+      return rows;
+  } catch (error) {
+    console.error("Error creating creatures!" + error);
+  }
 }
 
 module.exports = {
