1a886d05740f60e251eb06ef2b7cbfa53d479579
harley215 <harley215h@gmail.com>
2022-08-06 14:58:49 -0400

    feat: cart updated

---
+++ src/components/ShopCart.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from "react";
+import reactdomclient from "react-dom/client"
+import { BrowserRouter, useNavigate, useParams, Routes, Route, Link } from "react-router-dom";
+import butterbot from "../images/butterbot.png";
+function ShopCart(){
+    return (
+<p>Cart</p>
+    )
+}
+
+export default ShopCart;
