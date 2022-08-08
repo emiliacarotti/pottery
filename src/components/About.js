5f76cd12dc6c7797cfc7479d590480c5cfff4a6f
arich9504 <99704305+arich9504@users.noreply.github.com>
2022-08-07 22:11:22 -0400

    Style/#83 edits & fixes (#88)
    * feat: updating delete/patch
    
    * feat: add delete with :creatureid/singleItem
    
    * feat: add getCreatureById
    
    * fix: creatures route sp
    
    * fix: error message and values in createcreature
    
    * feat: added db cart_items and history_items match tables
    
    * feat:getCreatureById
    
    * feat: patch and delete creatures working
    
    * fix: fetch url home sp
    
    * feat: add onchange to select
    
    * bug fix
    
    * empty commit
    
    * for the love of god...
    
    * its freaking working!
    
    * firebase initiated
    
    * return to home link added
    
    * style: edit navbar
    
    * image route updated
    
    Co-authored-by: harley215 <harley215h@gmail.com>
    Co-authored-by: emiliacarotti <99696866+emiliacarotti@users.noreply.github.com>

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import reactdomclient from "react-dom/client"
+import { BrowserRouter, useNavigate, useParams, Routes, Route, Link } from "react-router-dom";
 
 function About(){
     
@@ -32,20 +34,20 @@ function About(){
                                         <p>Ittoqqortoormiit, Greenland. Oymyakon, Russia. Barrow, Alaska. Changtang, Tibet. Pyongyang, North Korea. Those are a few of the more remote locations that we have shipped our beast to. So in other words we've got you covered from pole to pole.</p>
                                     </div>
                                 </div>
-                        <div className="col-md-4">
-                            <div className="about-item">
-                                <i className="fa fa-helmet-safety"></i>
-                                <h3>Safety Measures</h3>
-                                <hr />
-                                <p>Ok, this is a big one. Please, please, please follow the safety recommendations shipped with your beast. If we suggest not to get your beast wet or feed it after midnight... it's probably best not to get it wet or feed it after midnight. These are rules for a reason folks. </p>
+                                <div className="col-md-4">
+                                    <div className="about-item">
+                                        <i className="fa fa-helmet-safety"></i>
+                                        <h3>Safety Measures</h3>
+                                        <hr/>
+                                        <p>Ok, this is a big one. Please, please, please follow the safety recommendations shipped with your beast. If we suggest not to get your beast wet or feed it after midnight... it's probably best not to get it wet or feed it after midnight. These are rules for a reason folks. </p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </div> 
                     </div>
                 </div>
-            </div>
-        </div>
-
-    </section>
+                
+            </section>
 }
 
 export default About;
