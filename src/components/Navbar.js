51fc9015c329ab0a1f81dd8eb75c77498be0c60a
emiliacarotti <emiliac@vt.edu>
2023-08-18 15:09:46 -0400

    230818-logo-title-fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,14 @@ export default function Navbar({ loggedIn, Logout, isAdmin, setIsAdmin }) {
     return (
         <>
             <Link to="/" className="title">
-                <div className="logo_image"><img className="home_image" src="https://firebasestorage.googleapis.com/v0/b/emilia-pottery.appspot.com/o/logo.png?alt=media&token=31b8a77b-fade-414b-a11b-7dca0b68cd09"></img></div>
+                <div className="title_image"><img className="home_image" src="https://firebasestorage.googleapis.com/v0/b/emilia-pottery.appspot.com/o/logo.png?alt=media&token=4debc1e6-7134-4c9c-bcaa-a0f7f05a44f2"></img></div>
             </Link>
+
 
             <div className="navbarLinks">
                 <a href="./"> Home </a>
                 <a href="./About"> About </a>
-                
+
                 {isAdmin == "true" ? (<a href="./Create"> Create a Listing </a>) : null}
                 {!loggedIn ? <a href="./Login"> Login </a> : <a href onClick={Logout}> Logout </a>}
             </div>
